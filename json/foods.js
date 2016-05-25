950e6e0e1aba57116d116523bb51b480a36e1e23
Hidayat Hasan (Dale) <daleaje45@gmail.com>
2016-05-25 17:27:00 -0500

    Uploading files from my FTP sites

---
+++ json/foods.js
@@ -0,0 +1,45 @@
+[
+{
+	"name":"Satay",
+	"desc":"Meat grilled on the stick",
+	"category":"appetizer",
+	"price":5,
+	"imageUrl": "img/1.jpg"
+},
+{
+	"name":"Soto",
+	"desc":"Soup with meats and coconut milk",
+	"category":"entree",
+	"price":7,
+	"imageUrl": "img/2.jpg"
+},
+{
+	"name":"Fried Chicken",
+	"desc":"Famous fried chicken with special sauce on the side",
+	"category":"entree",
+	"price":8,
+	"imageUrl": "img/3.jpg"
+},
+{
+	"name":"Fish Cake",
+	"desc":"Appetizer with fish paste and sweet sour sauce",
+	"category":"appetizer",
+	"price":4,
+	"imageUrl": "img/4.jpg"
+},
+{
+	"name":"Shaved Ice",
+	"desc":"Mixed fruit with syrup and shaved ice",
+	"category":"dessert",
+	"price":3,
+	"imageUrl": "img/5.jpg"
+},
+{
+	"name":"Choco cake",
+	"desc":"Mouth watering chocolate cake",
+	"category":"dessert",
+	"price":3,
+	"imageUrl": "img/6.jpg"
+}
+
+]
